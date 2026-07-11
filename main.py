9d1e76e3d9c7b6c9cf3e080087485f553e2fe7b8
Rosvelt Dumpit <ross01_online@yahoo.com>
2026-07-11 08:09:49 +0100

    fix main.py guard

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+# import transform
+# import load
+# import ingest
+
+
+# data, content_type = ingest.extract()
+# df, content_type = transform.transform(data, content_type)
+# load.load(df)
+
+# print("Load complete")
+
+# if __name__ == "__main__":
+#     df, content_type = transform.transform(data, content_type)
+#     load.load(df)
+#     print("Load complete")
+
 import transform
 import load
 import ingest
 
-
-data, content_type = ingest.extract()
-df, content_type = transform.transform(data, content_type)
-load.load(df)
-
-print("Load complete")
+if __name__ == "__main__":
+    data, content_type = ingest.extract()
+    df, content_type = transform.transform(data, content_type)
+    load.load(df)
+    print("Load complete")
